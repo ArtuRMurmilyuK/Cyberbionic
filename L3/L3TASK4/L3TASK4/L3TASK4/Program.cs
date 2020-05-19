a158cac9d16479bd0225f0113882359f867f4dfd
ArtuRMurmilyuK <murmilyuk.artur20@gmail.com>
2020-05-19 20:47:45 +0300

    Change the code in L3TASK4

--- L3/L3TASK4/L3TASK4/L3TASK4/Program.cs
+++ L3/L3TASK4/L3TASK4/L3TASK4/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Runtime.Remoting.Services;
 
 namespace L3TASK4
 {
@@ -6,6 +7,34 @@ namespace L3TASK4
     {
         static void Main()
         {
+            int a, b;
+            Console.WriteLine("Enter a:");
+            a=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter b:");
+            b = Convert.ToInt32(Console.ReadLine());
+
+            int sum = a + b;
+            Console.WriteLine("sum:" + sum);
+
+            int sub = a - b;//subtraction
+            Console.WriteLine("subtraction:" +sub);
+
+            int mul = a * b; //multiplication
+            Console.WriteLine("multiplication:" +mul);
+
+            int div = a / b; //division
+            Console.WriteLine("division:" + div);
+
+            int rem = a % b; //remainder of integer division
+            Console.WriteLine("remainder of integer division: " + rem);
+
+            int inc = ++a ; //Increment operation
+            Console.WriteLine("increment operation(++): " + inc +", "+ (++b));
+
+            int incm = --a; //Increment operation minus
+            Console.WriteLine("increment operation(--): " + incm + ", " + (--b));
+
+            Console.ReadKey();
         }
     }
 }
