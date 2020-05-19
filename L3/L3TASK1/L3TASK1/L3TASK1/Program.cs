9a1a2a639a9f3e647ee8844b6feb0edd052562a7
ArtuRMurmilyuK <murmilyuk.artur20@gmail.com>
2020-05-19 16:56:20 +0300

    Change the code in L3TASK1

--- L3/L3TASK1/L3TASK1/L3TASK1/Program.cs
+++ L3/L3TASK1/L3TASK1/L3TASK1/Program.cs
@@ -7,13 +7,32 @@ namespace L3TASK1
         static void Main()
         {
 
-            int x=10, y=12, z=3;
+            {
+                int x = 10, y = 12, z = 3;
+                Console.WriteLine(x += y - x++ * z);//10+12-10*3=22-30=-8
+            }
+
+
+            {
+                int x = 10, y = 12, z = 3;
+                Console.WriteLine(z = --x - y * 5);//9-12*5=9-60
+            }
+
+            {
+                int x = 10, y = 12, z = 3;
+                Console.WriteLine(y /= x + 5 % z);//12/12=1
+            }
+
+            {
+                int x = 10, y = 12, z = 3;
+                Console.WriteLine(z = x++ + y * 5);//10 + 60 = 70
+            }
+
+            {
+                int x = 10, y = 12, z = 3;
+                Console.WriteLine(x = y - x++ * z);//12 - 30=-18
+            }
             
-            Console.WriteLine( x += y - x++ * z);
-            Console.WriteLine(z = --x - y * 5);
-            Console.WriteLine(y /= x + 5 % z);
-            Console.WriteLine(z = x++ + y * 5);
-            Console.WriteLine(x = y - x++ * z);
             Console.ReadKey();
 
         }
