a55c250b512f3225549f1bcc22d98e19a753a027
ArtuRMurmilyuK <murmilyuk.artur20@gmail.com>
2020-05-19 12:03:56 +0300

    Added L2TASK3

---
+++ L2/L2TASK3/L2TASK3WPF.APP/L2TASK3WPF.APP/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿
+using System.Windows;
+
+
+namespace L2TASK3WPF.APP
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_byte(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("from 0 to 255");
+        }
+
+        private void Button_sbyte(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("from -128 to +127");
+        }
+
+        private void Button_short(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("from -32768 to +32767");
+        }
+
+        private void Button_ushort(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("from 0 to 65535");
+        }
+
+        private void Button_int(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("from -2147483648 to +2147483647");
+        }
+
+        private void Button_uint(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("from 0 to 4294967295");
+        }
+
+        private void Button_long(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("from -9223372036854775808 to +9223372036854775807");
+        }
+
+        private void Button_ulong(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("from 0 to 18446744073709551615");
+        }
+    }
+}
---
+++ L2/L2TASK3/README.txt
@@ -0,0 +1,16 @@
+Task 3
+
+Using Visual Studio, create a project using the Windows Forms template. 
+Place 8 buttons with the names of integer types on the form.
+The following code is MessageBox.Show ("text can be entered here"); and execute the program. 
+After all the execution programs, delete the lines - here you can specify valid values ??for the corresponding types. 
+Launch the app.
+
+PLAN
+1.create a project using the Windows Forms template
+2.delete using/reference
+3.Place 8 buttons with the names of integer types on the form.
+4.write Next code  - MessageBox.Show("here you can whrite text")
+5.run the program
+6.change data in MessageBox
+7.Run the program
