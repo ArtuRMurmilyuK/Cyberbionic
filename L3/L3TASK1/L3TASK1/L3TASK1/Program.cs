959d3e8f8e61536e29518bedc17e37f3f7ec141f
ArtuRMurmilyuK <murmilyuk.artur20@gmail.com>
2020-05-19 16:17:25 +0300

    Added code in L3TASK1

--- L3/L3TASK1/L3TASK1/L3TASK1/Program.cs
+++ L3/L3TASK1/L3TASK1/L3TASK1/Program.cs
@@ -1,12 +1,21 @@
 ﻿using System;
 
-
 namespace L3TASK1
 {
     class Program
     {
         static void Main()
         {
+
+            int x=10, y=12, z=3;
+            
+            Console.WriteLine( x += y - x++ * z);
+            Console.WriteLine(z = --x - y * 5);
+            Console.WriteLine(y /= x + 5 % z);
+            Console.WriteLine(z = x++ + y * 5);
+            Console.WriteLine(x = y - x++ * z);
+            Console.ReadKey();
+
         }
     }
 }
