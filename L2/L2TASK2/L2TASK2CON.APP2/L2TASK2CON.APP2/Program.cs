921e91d00424a691bd7b1178ceb0d4b4b7b1241d
ArtuRMurmilyuK <murmilyuk.artur20@gmail.com>
2020-05-19 11:02:07 +0300

    Added L2TASK2

---
+++ L2/L2TASK2/L2TASK2CON.APP2/L2TASK2CON.APP2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace L2TASK2CON.APP2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string FirstString = "\nMy string 1";
+            string SecondString = "\tMy string 2";
+            string ThirdString = "\aMy string 3";
+
+            Console.WriteLine(FirstString);
+            Console.WriteLine(SecondString);
+            Console.WriteLine(ThirdString);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ L2/L2TASK2/README.txt
@@ -0,0 +1,10 @@
+Task 2
+Using Visual Studio, create a project using the Console Application template.
+Create three string variables and assign values ??to them: "\nmy string 1" "\ tmy string 2" "\amy string 3"
+Display the value of each variable on the screen. What differences did you see. Draw conclusions.
+
+Plan
+1.create a project using the Console Application
+2. Delete using/reference
+3.create 3 variables 
+4.display them on the screen
