3dd12771bbd17763fe0423592d5e8362a729ee48
ArtuRMurmilyuK <murmilyuk.artur20@gmail.com>
2020-05-20 21:31:25 +0300

    Changed code in L4TASK3

--- L4/L4TASK3/L4TASK3/L4TASK3/Program.cs
+++ L4/L4TASK3/L4TASK3/L4TASK3/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 
 
 namespace L4TASK3
@@ -7,6 +8,78 @@ namespace L4TASK3
     {
         static void Main()
         {
+            Console.WriteLine("Select word for translate :Дождь, Снег, Солнце, Облачно, Град, Шторм, Потоп, Безоблачный, Ветер, Гололёд");
+            string words= Console.ReadLine();
+            switch (words)
+            {
+                case "Дождь":
+                {
+                    Console.WriteLine("Rain");
+                    Console.ReadKey();
+                    break;
+                }
+                case "Снег":
+                {
+                    Console.WriteLine("Snow");
+                    Console.ReadKey();
+                    break;
+                }
+                case "Солнце":
+                {
+                    Console.WriteLine("The sun");
+                    Console.ReadKey();
+                    break;
+                }
+                case "Облачно":
+                {
+                    Console.WriteLine("Cloudy");
+                    Console.ReadKey();
+                    break;
+                }
+                case "Град":
+                {
+                    Console.WriteLine("Hail");
+                    Console.ReadKey();
+                    break;
+                }
+                case "Шторм":
+                {
+                    Console.WriteLine("Storm");
+                    Console.ReadKey();
+                    break;
+                }
+                case "Потоп":
+                {
+                    Console.WriteLine("Deluge");
+                    Console.ReadKey();
+                    break;
+                }
+                case "Безоблачный":
+                {
+                    Console.WriteLine("Cloudless");
+                    Console.ReadKey();
+                    break;
+                }
+                case "Ветер":
+                {
+                    Console.WriteLine("Wind");
+                    Console.ReadKey();
+                    break;
+                }
+                case "Гололёд":
+                {
+                    Console.WriteLine("Ice");
+                    Console.ReadKey();
+                    break;
+                }
+                default:
+                {
+                    Console.WriteLine("This word is not in the dictionary.");
+                    Console.ReadKey();
+                    break;
+                }
+
+            }
         }
     }
 }
