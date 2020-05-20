bba0c820e46925d7dfc8a6b704b52a583465ae46
ArtuRMurmilyuK <murmilyuk.artur20@gmail.com>
2020-05-20 19:16:03 +0300

    add code in L4TASK2

--- L4/L4TASK2/L4TASK2/Program.cs
+++ L4/L4TASK2/L4TASK2/Program.cs
@@ -6,6 +6,49 @@ namespace L4TASK2
     {
         static void Main()
         {
+            float number;
+            Console.WriteLine("Enter number 0-100:");
+            string input = Console.ReadLine();
+
+            bool result = float.TryParse(input, out number);
+            if (result == true )
+            {
+                if (number>=0 && number <=100)
+                {
+                    if (number >= 0 && number <= 14)
+                    {
+                        Console.WriteLine("Range [0-14]");
+                        Console.ReadKey();
+                    }
+                    else if(number >= 15 && number <= 35)
+
+                    {
+                        Console.WriteLine("Range [15-35]");
+                        Console.ReadKey();
+                    }
+                    else if (number >= 36 && number <= 49)
+                    {
+                        Console.WriteLine("Range [36-49] ");
+                        Console.ReadKey();
+                    }
+                    else if (number >= 50 && number <= 100)
+                    {
+                        Console.WriteLine("Range [50-100] ");
+                        Console.ReadKey();
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("ERROR\nIncorrect number entered");
+                    Console.ReadKey();
+                }
+            }
+
+            else
+            {
+                Console.WriteLine("No number entered");
+                Console.ReadKey();
+            }
         }
     }
 }
