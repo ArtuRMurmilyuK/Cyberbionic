a37b4cd59494a890fb806caae48b819c5e7d7cfc
ArtuRMurmilyuK <murmilyuk.artur20@gmail.com>
2020-05-21 21:46:43 +0300

    Changed code in L5TASK2

--- L5/L5TASK2/L5TASK2/Program.cs
+++ L5/L5TASK2/L5TASK2/Program.cs
@@ -6,6 +6,37 @@ namespace L5TASK2
     {
         static void Main()
         {
+            {
+                int x = 5, y = 10, z = 15;
+                int result = x += y >> x++ * z; //5 * 15 = 75
+                Console.WriteLine("Result x = " + result);
+            }
+
+            {
+                int x = 5, y = 10, z = 15;
+                int result = z = ++x & y * 5;
+                Console.WriteLine("Result z = " + result);
+            }
+
+            {
+                int x = 5, y = 10, z = 15;
+                int result = y /= x + 5 | z;
+                Console.WriteLine("Result y = " + result);
+            }
+
+            {
+                int x = 5, y = 10, z = 15;
+                int result = z = x++ & y * 5;
+                Console.WriteLine("Result z = " + result);
+            }
+
+            {
+                int x = 5, y = 10, z = 15;
+                int result = x = y << x++ ^ z;
+                Console.WriteLine("Result x = " + result);
+                Console.ReadKey();
+            }
+
         }
     }
 }
