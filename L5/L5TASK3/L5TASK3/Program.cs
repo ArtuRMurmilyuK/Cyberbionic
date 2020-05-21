8f77b6051877013fa27222f8c2af87757eecc187
ArtuRMurmilyuK <murmilyuk.artur20@gmail.com>
2020-05-22 01:38:35 +0300

    Changed code in L5TASK3

--- L5/L5TASK3/L5TASK3/Program.cs
+++ L5/L5TASK3/L5TASK3/Program.cs
@@ -6,6 +6,69 @@ namespace L5TASK3
     {
         static void Main()
         {
+            int age;
+            double prize, salary;
+
+            Console.WriteLine("Enter your salary");
+            string input1  = Console.ReadLine();
+
+            bool result = double.TryParse(input1, out salary);
+            if (result == true)
+            {
+                Console.WriteLine("Enter the years of service");
+                string input = Console.ReadLine();
+
+                bool result1 = int.TryParse(input, out age);
+                if (result1 == true)
+                {
+                    if (age >= 0 && age < 5)
+                    {
+                        prize = salary * 10 / 100;
+                        Console.WriteLine(prize);
+                        Console.ReadKey();
+                    }
+                    else if(age >= 5 && age < 10)
+                    {
+                        prize = salary * 15 / 100;
+                        Console.WriteLine(prize);
+                        Console.ReadKey();
+                    }
+                    else if (age >= 10 && age < 15)
+                    {
+                        prize = salary * 25 / 100;
+                        Console.WriteLine(prize);
+                        Console.ReadKey();
+                    }
+                    else if (age >= 15 && age < 20)
+                    {
+                        prize = salary * 35 / 100;
+                        Console.WriteLine(prize);
+                        Console.ReadKey();
+                    }
+                    else if (age >= 20 && age < 25)
+                    {
+                        prize = salary * 45 / 100;
+                        Console.WriteLine(prize);
+                        Console.ReadKey();
+                    }
+                    else if (age >= 25)
+                    {
+                        prize = salary * 50 / 100;
+                        Console.WriteLine(prize);
+                        Console.ReadKey();
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Not entered number");
+                    Console.ReadKey();
+                }
+            }
+            else
+            {
+                Console.WriteLine("Not entered number");
+                Console.ReadKey();
+            }
         }
     }
 }
--- L5/L5TASK3/README.txt
+++ L5/L5TASK3/README.txt
@@ -1,17 +1,17 @@
 TASK 3
 
--Using Visual Studio, create a project using the ConsoleApplication template. 
--Write a program for calculating employee bonuses. 
--Bonuses are calculated according to the length of service. 
--If the length of service is up to 5 years, the bonus is 10% of the salary. 
--If the length of service is from 5 years (inclusive) to 10 years, the premium is 15% of the salary. 
--If the length of service is from 10 years (inclusive) to 15 years, the premium is 25% of the salary. 
--If the length of service is from 15 years (inclusive) to 20 years, the premium is 35% of the salary. 
--If the length of service is from 20 years (inclusive) to 25 years, the premium is 45% of the salary. 
--If the length of service is 25 years (inclusive) or more, the premium is 50% of the salary. 
--Calculation results, display.
++Using Visual Studio, create a project using the ConsoleApplication template. 
++Write a program for calculating employee bonuses. 
++Bonuses are calculated according to the length of service. 
++If the length of service is up to 5 years, the bonus is 10% of the salary. 
++If the length of service is from 5 years (inclusive) to 10 years, the premium is 15% of the salary. 
++If the length of service is from 10 years (inclusive) to 15 years, the premium is 25% of the salary. 
++If the length of service is from 15 years (inclusive) to 20 years, the premium is 35% of the salary. 
++If the length of service is from 20 years (inclusive) to 25 years, the premium is 45% of the salary. 
++If the length of service is 25 years (inclusive) or more, the premium is 50% of the salary. 
++Calculation results, display.
 
 Plan
--create a project using the ConsoleApplication
--If the length of service is up to 5 years, the bonus is 10% of the salary. 
--Output Result
++create a project using the ConsoleApplication
++If the length of service is up to 5 years, the bonus is 10% of the salary. 
++Output Result
