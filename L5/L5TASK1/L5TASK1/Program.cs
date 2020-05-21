eac2e27943ff87f927440fd735c39d9ae68d5795
ArtuRMurmilyuK <murmilyuk.artur20@gmail.com>
2020-05-21 21:18:22 +0300

    Parity Check (Added code L5TASK1)

--- L5/L5TASK1/L5TASK1/Program.cs
+++ L5/L5TASK1/L5TASK1/Program.cs
@@ -6,6 +6,39 @@ namespace L5TASK1
     {
         static void Main()
         {
+            int number;
+            Console.WriteLine("Enter a parity number:");
+            string input = Console.ReadLine();
+
+            bool result = int.TryParse(input, out number);
+            if (result == true)
+            {
+                if (number % 2 == 0)
+                {
+                    Console.WriteLine("The number {0} is even", number);
+                }
+                else
+                {
+                    Console.WriteLine("Odd number{0}", number);
+                }
+                //Second metod
+                if ((number & 1) == 0)
+                {
+                    Console.WriteLine("The number {0} is even", number);
+                    Console.ReadKey();
+                }
+                else
+                {
+                    Console.WriteLine("Odd number{0}", number);
+                    Console.ReadKey();
+                }
+            }
+
+            else
+            {
+                Console.WriteLine("No number entered");
+                Console.ReadKey();
+            }
         }
     }
 }
--- L5/L5TASK1/README.txt
+++ L5/L5TASK1/README.txt
@@ -1,11 +1,11 @@
--Task 1
--It is known that for even numbers, the least significant bit has the value 0. 
--Using Visual Studio, create a project using the ConsoleApplication template. 
--Write a program that will check the numbers for parity. 
--Suggest two options for solving the problem.
++Task 1
++It is known that for even numbers, the least significant bit has the value 0. 
++Using Visual Studio, create a project using the ConsoleApplication template. 
++Write a program that will check the numbers for parity. 
++Suggest two options for solving the problem.
 
 Plan
--create a project using the ConsoleApplication template
--Make a parity check
--1 option Through %
--2 ???
++create a project using the ConsoleApplication template
++Make a parity check
++1 option Through %
++2 & metod
