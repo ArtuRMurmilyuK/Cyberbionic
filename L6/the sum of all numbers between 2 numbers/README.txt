64c8c396f84999c3fe19c766c874f74fc0199723
ArtuRMurmilyuK <murmilyuk.artur20@gmail.com>
2020-05-22 12:06:16 +0300

    Changed code in the sum of all numbers between 2 numbers

--- L6/the sum of all numbers between 2 numbers/README.txt
+++ L6/the sum of all numbers between 2 numbers/README.txt
@@ -1,10 +1,10 @@
-﻿-TASK 1
--Using Visual Studio, create a project using the Console Application template. 
--Given two numbers A and B (A <B) print the sum of all the numbers located between these numbers on the screen. 
--Given two numbers A and B (A <B) print all the odd values ​​located between the given numbers.
-
--Plan
--create a project using the Console Application template
--make input 2 A and B
--print the sum of all the numbers located between these numbers on the screen
--print all the odd values ​​located between the given numbers
+﻿+TASK 1
++Using Visual Studio, create a project using the Console Application template. 
++Given two numbers A and B (A <B) print the sum of all the numbers located between these numbers on the screen. 
++Given two numbers A and B (A <B) print all the odd values ​​located between the given numbers.
++
++Plan
++create a project using the Console Application template
++make input 2 A and B
++print the sum of all the numbers located between these numbers on the screen
++print all the odd values ​​located between the given numbers
--- L6/the sum of all numbers between 2 numbers/the sum of all numbers between 2 numbers/Program.cs
+++ L6/the sum of all numbers between 2 numbers/the sum of all numbers between 2 numbers/Program.cs
@@ -6,6 +6,53 @@ namespace the_sum_of_all_numbers_between_2_numbers
     {
         static void Main()
         {
+            int A;
+            Console.WriteLine("Enter A:");
+            string valueA = Console.ReadLine();
+
+            bool checkA = int.TryParse(valueA, out A);
+            if (checkA == true)
+            {
+                int B;
+                Console.WriteLine("Enter B:");
+                string valueB = Console.ReadLine();
+
+                bool checkB = int.TryParse(valueB, out B);
+                if (checkB == true)
+                {
+                    if (A < B)
+                    {
+                        int sum = 0;
+                        for (int num = ++A; num < B; num++)
+                        {
+
+                            sum += num;
+                        }
+                        Console.WriteLine("Result sum = " + sum);
+                    }
+
+                    if (A < B)
+                    {
+                        for (int num = ++A; num < B; num++)
+                        {
+                            if (num % 2 == 0)
+                            {
+                                Console.WriteLine("Odd numbers = " + num);
+                            }
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Error\nB<A");
+                        Console.ReadKey();
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("No number entered");
+                Console.ReadKey();
+            }
         }
     }
 }
