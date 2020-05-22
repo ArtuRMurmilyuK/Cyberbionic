c36cc7c89b2d84b1bfaf7456ed29c8862ec6ddfa
ArtuRMurmilyuK <murmilyuk.artur20@gmail.com>
2020-05-22 09:09:39 +0300

    Changed code in L5TASK4(Teor. De Morgan)

--- L5/L5TASK4/L5TASK4/Program.cs
+++ L5/L5TASK4/L5TASK4/Program.cs
@@ -6,6 +6,32 @@ namespace L5TASK4
     {
         static void Main()
         {
+            //A | B = !( !A & !B)  
+            bool A = true;
+            bool B = false;
+
+            // before applying the theorem
+            if (A | B)
+            {
+                Console.WriteLine("A | B = {0}", A | B);
+            }
+            else
+            {
+                Console.WriteLine("A | B = {0}", A | B);
+            }
+
+            // After applying the theorem
+            if (!(!A & !B))
+            {
+                Console.WriteLine("!(!A & !B) = {0}", !(!A & !B));
+            }
+            else
+            {
+                Console.WriteLine("!(!A & !B) = {0}", !(!A & !B));
+            }
+
+            // Delay.
+            Console.ReadKey();
         }
     }
 }
