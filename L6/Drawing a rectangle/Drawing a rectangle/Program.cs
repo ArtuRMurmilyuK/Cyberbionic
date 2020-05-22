be64c3aa27c719793de0a0ed3e483b165811127c
ArtuRMurmilyuK <murmilyuk.artur20@gmail.com>
2020-05-22 16:57:08 +0300

    Create code in Drawing a reclangle

--- L6/Drawing a rectangle/Drawing a rectangle/Program.cs
+++ L6/Drawing a rectangle/Drawing a rectangle/Program.cs
@@ -4,8 +4,31 @@ namespace Drawing_a_rectangle
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            bool check = false;
+
+            int I= 0, J = 0;
+            do
+            {
+                Console.WriteLine("Enter width:");
+                string i = Console.ReadLine();
+                check = int.TryParse(i, out I);
+                Console.WriteLine("Enter width:");
+                string j = Console.ReadLine();
+                check = int.TryParse(j, out J);
+            } while (check == false);
+
+            int n = 0, m = 0;
+            for (n = 0; n < I; n++)
+            {
+                for (m = 0; m < J; m++ )
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
         }
     }
 }
--- L6/Drawing a rectangle/README.txt
+++ L6/Drawing a rectangle/README.txt
@@ -1,10 +1,10 @@
-﻿-TASK 4
--Using Visual Studio, create a project using the Console Application template. 
--Create two integer variables and give them some values. 
--Using the technique of nested loops, draw a rectangle of stars. 
--Use the values ​​of previously created variables to indicate the height and width of the rectangle.
+﻿+TASK 4
++Using Visual Studio, create a project using the Console Application template. 
++Create two integer variables and give them some values. 
++Using the technique of nested loops, draw a rectangle of stars. 
++Use the values ​​of previously created variables to indicate the height and width of the rectangle.
 
--Plan
--Create 2 variable
--Using for
--Draw a rectangle
++Plan
++Create 2 variable
++Using for
++Draw a rectangle
