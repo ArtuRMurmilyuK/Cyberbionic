4e665abf5fde630a01a84c81e608238b4ad56fe0
ArtuRMurmilyuK <murmilyuk.artur20@gmail.com>
2020-05-25 12:57:35 +0300

    Changed code in the Currency conversion

--- L7/Currency conversion/Currency conversion/Program.cs
+++ L7/Currency conversion/Currency conversion/Program.cs
@@ -4,8 +4,40 @@ namespace Currency_conversion
 {
     class Program
     {
+        static double Conventional_money(double a, double b)
+        {
+            return  a / b;
+        }
         static void Main()
         {
+            var money = 0d;
+            var course = 0d;
+            var result = 0m;
+            string carentlyAfter = null;
+            Console.WriteLine("Enter the currency you want to exchange:");
+            var carentlyBefore = Console.ReadLine();
+
+            Console.WriteLine("Enter the amount of money:");
+            var stringMoney = Console.ReadLine();
+            var checkMoney = double.TryParse(stringMoney, out money);
+            if (checkMoney)
+            {
+                Console.WriteLine("Enter what currency you want to receive:");
+                carentlyAfter = Console.ReadLine();
+                
+
+                Console.WriteLine("Enter the rate to convert to another currency:");
+                var stringCourse = Console.ReadLine();
+                var check = double.TryParse(stringCourse, out course);
+                if (check)
+                {
+                    result =  (decimal) Conventional_money(money, course);
+                }
+            }
+
+            Console.WriteLine("{0} {1}", result, carentlyAfter);
+            Console.ReadKey();
         }
+
     }
 }
--- L7/Currency conversion/README.txt
+++ L7/Currency conversion/README.txt
@@ -1,11 +1,11 @@
--Task 2
--Using Visual Studio, create a project using the ConsoleApplication template. 
--Write a program that will convert currencies. 
--The user enters: the amount of money in a certain currency. 
--rate to convert to another currency. 
--Organize the output of the currency conversion operation on the screen.
++Task 2
++Using Visual Studio, create a project using the ConsoleApplication template. 
++Write a program that will convert currencies. 
++The user enters: the amount of money in a certain currency. 
++rate to convert to another currency. 
++Organize the output of the currency conversion operation on the screen.
 
--Plan
--Enter money
--Enter exchange rate to another currency
--Display
++Plan
++Enter money
++Enter exchange rate to another currency
++Display
