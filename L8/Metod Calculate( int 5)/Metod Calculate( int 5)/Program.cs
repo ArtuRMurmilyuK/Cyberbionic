7b2c288e677164e6332b63b57e4de9a7735390ea
ArtuRMurmilyuK <murmilyuk.artur20@gmail.com>
2020-05-26 16:48:22 +0300

    Changed code in Metod calculate

--- L8/Metod Calculate( int 5)/Metod Calculate( int 5)/Program.cs
+++ L8/Metod Calculate( int 5)/Metod Calculate( int 5)/Program.cs
@@ -4,8 +4,36 @@ namespace Metod_Calculate__int_5_
 {
     class Program
     {
+        static int EnterValue(int num)
+        {
+            var stringNum = Console.ReadLine();
+            var check = int.TryParse(stringNum, out num);
+            if (check)
+            {
+                return num;
+            }
+            else return EnterValue(num);
+        }
+
+        static (int, int, int) Calculate(int firstNum, int secondNum, int thirdNum)
+        {
+            return (firstNum /= 5, secondNum /= 5, thirdNum /= 5);
+        }
         static void Main()
         {
+            var firstNum = 0;
+            var secondNum = 0;
+            var thirdNum = 0;
+
+            Console.WriteLine("Enter first number:");
+            firstNum = EnterValue(firstNum);
+            Console.WriteLine("Enter second number:");
+            secondNum = EnterValue(secondNum);
+            Console.WriteLine("Enter third number:");
+            thirdNum = EnterValue(thirdNum);
+            Console.WriteLine(Calculate(firstNum, secondNum, thirdNum));
+            Console.ReadKey();
+
         }
     }
 }
