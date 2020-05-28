8ff8612e36e724c39a92f24cacb71bbb995a1f51
ArtuRMurmilyuK <murmilyuk.artur20@gmail.com>
2020-05-28 22:35:42 +0300

    Changed code in Metod MyReverse

--- L9/Metod MyReverse/Metod MyReverse/Program.cs
+++ L9/Metod MyReverse/Metod MyReverse/Program.cs
@@ -4,8 +4,74 @@ namespace Metod_MyReverse
 {
     class Program
     {
-        static void Main(string[] args)
+        static void MyReverse(int[] array,  int n)
         {
+            int[] modArray = new int[array.Length];
+            for (int i = 0; i <= array.Length; i++)
+            {
+                n--;
+                modArray[i] = array[n];
+                Console.WriteLine( modArray[i] );
+                if (n == 0)
+                {
+                    break;
+                }
+            }
+            modArray = array;
+        }
+        static int [] SubArray(int[] array, int index, int count)
+        {
+           
+            int [] modArray = new int[count];
+            for (int i = 0; i < count; i++)
+            {
+                if (modArray[i] >= array.Length - index)
+                {
+                    modArray[i] = 1;
+                }
+                else
+                {
+                    modArray[i] = array[index];//1,2,3,4,5
+                }
+                index++;
+                Console.WriteLine(modArray[i]);
+            }
+            modArray = array;
+            return array;
+        }
+        static void Main()
+        {
+            Console.WriteLine("Enter N");
+            var stringN = Console.ReadLine();
+            if (int.TryParse(stringN, out int n))
+            {
+                    int[] array = new int[n];
+
+                    for (int i = 0; i < array.Length; i++)
+                    {
+                        var stringArray = Console.ReadLine();
+                        if (int.TryParse(stringArray, out array[i])) ;
+                    }
+
+                    for (int i = 0; i < array.Length; i++)
+                    {
+                        Console.WriteLine(array[i]);
+                    }
+
+                    Console.WriteLine("Enter index:");
+                    var stringIndex = Console.ReadLine();
+                    if (int.TryParse(stringIndex, out int index))
+                    {
+                        Console.WriteLine("Enter count");
+                        var stringCount = Console.ReadLine();
+                        if (int.TryParse(stringCount, out int count))
+                        {
+                            MyReverse(array, n);
+                            SubArray(array, index, count);
+                        }
+                    }
+            }
+            Console.ReadKey();
         }
     }
 }
--- L9/Metod MyReverse/README.txt
+++ L9/Metod MyReverse/README.txt
@@ -1,13 +1,13 @@
-TASK
-Using Visual Studio, create a project using the ConsoleApplication template. 
-Required: 1) Create a method MyReverse (int [] array), 
-which takes an array of integer elements as an argument and returns an inverted array (array elements in reverse order). 
-2) Create an int [] SubArray method (int [] array, int index, int count). 
-The method returns the part of the array received as an argument, starting from the position specified in the index argument, 
-with a dimension that corresponds to the value of the count argument.
-If the argument count contains a value greater than the number of elements that are included in the selectable part of the source array (from the specified index to the index of the last element), 
-then when forming a new array with dimension in count, fill in the units with those elements that were not copied from the original array 
++TASK
++Using Visual Studio, create a project using the ConsoleApplication template. 
++Required: 1) Create a method MyReverse (int [] array), 
++which takes an array of integer elements as an argument and returns an inverted array (array elements in reverse order). 
++2) Create an int [] SubArray method (int [] array, int index, int count). 
++The method returns the part of the array received as an argument, starting from the position specified in the index argument, 
++with a dimension that corresponds to the value of the count argument.
++If the argument count contains a value greater than the number of elements that are included in the selectable part of the source array (from the specified index to the index of the last element), 
++then when forming a new array with dimension in count, fill in the units with those elements that were not copied from the original array 
 
-Plan
-create metod MyReverse
-Create metod SubArray
++Plan
++create metod MyReverse
++Create metod SubArray
