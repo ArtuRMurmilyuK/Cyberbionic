4ea88d2350460fcee8e1fc935eeed3c8164c63c4
ArtuRMurmilyuK <murmilyuk.artur20@gmail.com>
2020-08-11 21:19:30 +0300

    CalcTests

---
+++ Essential/MyCalc/MyCalc/MyCalc.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace MyCalc
+{
+    public class MyCalc
+    {
+        static void Main()
+        {
+            
+        }
+
+        public int Sum(int x, int y)
+        {
+            return x + y;
+        }
+    }
+}
---
+++ Essential/MyCalc/MyCalcTests/MyCalcTests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyCalc;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyCalc.Tests
+{
+    [TestClass()]
+    public class MyCalcTests
+    {
+        [TestMethod()]
+        public void SumTest()
+        {
+            int x = 10;
+            int y = 20;
+            int expected = 30;
+
+            MyCalc c = new MyCalc();
+            int actual = c.Sum(x, y);
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
