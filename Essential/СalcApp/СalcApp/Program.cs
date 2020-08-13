c40520de26cfaa9469a7d0ef6cadb9dd27c120ea
ArtuRMurmilyuK <murmilyuk.artur20@gmail.com>
2020-08-13 14:19:23 +0300

    CalcTests(_target)

---
+++ Essential/СalcApp/СalcApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace СalcApp
+{
+    class Program
+    {
+        static void Main()
+        {
+        }
+    }
+
+    public class Calc
+    {
+        public int Sum(int x, int y)
+        {
+            return x + y;
+        }
+    }
+}
---
+++ Essential/СalcApp/СalcAppTests/CalcTests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using СalcApp;
+
+namespace СalcAppTests
+{
+    [TestClass]
+    public class CalcTests
+    {
+        private Calc _target;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _target = new Calc();
+        }
+
+        [TestMethod]
+        public void SumTest()
+        {
+            var actual = _target.Sum(10, 20);
+            Assert.AreEqual(30, actual);
+        }
+    }
+}
