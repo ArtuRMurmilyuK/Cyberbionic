2dd97d42a629b9aa3e324f9239c050b9b74a36f6
ArtuRMurmilyuK <murmilyuk.artur20@gmail.com>
2020-08-14 14:25:13 +0300

    AreaOfRectangle  Tests

--- Essential/AreaOfARectangle/AreaOfARectangle/Rectangle.cs
+++ Essential/AreaOfARectangle/AreaOfARectangle/Rectangle.cs
@@ -1,12 +1,20 @@
 ﻿namespace AreaOfARectangle
 {
+    // <summary>
+    // This class computes Area and Perimeter
+    // </summary>
     public class Rectangle
     {
+        // <summary>
+        // initialized with argument values
+        // </summary>
         private readonly double _side1;
         private readonly double _side2;
 
+        // <summary>
+        // Access modifier
+        // </summary>
         public double Area => AreaCalculator(_side1, _side2);
-
         public double Perimeter => PerimeterCalculator(_side1, _side2);
 
         public Rectangle(double side1, double side2)
@@ -14,11 +22,17 @@
             _side1 = side1;
             _side2 = side2;
         }
+
+        // <summary>
+        // Calculate the area
+        // </summary>
         public double AreaCalculator(double side1, double side2)
         {
             return side1 * side2;
         }
-
+        // <summary>
+        // Calculates the perimeter
+        // </summary>
         public double PerimeterCalculator(double side1, double side2)
         {
             return side1 + side1 + side2 + side2;
---
+++ Essential/AreaOfARectangle/AreaOfARectangleTests/RectangleTests.cs
@@ -0,0 +1,31 @@
+﻿using Xunit;
+using AreaOfARectangle;
+
+namespace AreaOfARectangleTests
+{
+    public class RectangleTests
+    {
+        // init target 
+        private readonly Rectangle _target;
+
+        public RectangleTests()
+        {
+            _target = new Rectangle(2, 3);
+        }
+
+        //Test method PerimeterCalculator
+        [Fact]
+        public void PerimeterCalculatorTest()
+        {
+            var actual = _target.PerimeterCalculator(2, 3);
+            Assert.Equal(10, actual);
+        }
+        //Test method AreaCalculator
+        [Fact]
+        public void AreaCalculatorTests()
+        {
+            var actual = _target.AreaCalculator(2, 3);
+            Assert.Equal(6, actual);
+        }
+    }
+}
