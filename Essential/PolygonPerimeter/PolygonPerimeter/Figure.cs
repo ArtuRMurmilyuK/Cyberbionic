2dae149f6e0bdd4cae8f3665bb93b534808d3836
ArtuRMurmilyuK <murmilyuk.artur20@gmail.com>
2020-08-19 12:15:13 +0300

    PolygonPerimetr figure

--- Essential/PolygonPerimeter/PolygonPerimeter/Figure.cs
+++ Essential/PolygonPerimeter/PolygonPerimeter/Figure.cs
@@ -4,19 +4,31 @@ namespace PolygonPerimeter
 {
     class Figure
     {
+        private Point[] points;
         //Должен принимать Аргументы типа Point 
         public Figure()
         {
-
+            Console.WriteLine("enter number of vertices of your figure");
+            var number = int.Parse(Console.ReadLine());
+            for (int i = 0; i < number; i++)
+            {
+                points[i] = new Point();
+            }
         }
-        double LengthSide(Point A, Point B)
+        double LengthSide(Point a, Point b)
         {
-            return Math.Sqrt(Math.Pow(A.X - B.X, 2) + Math.Pow(A.Y - B.Y, 2));
+            return Math.Sqrt(Math.Pow(a.X - b.X, 2) + Math.Pow(a.Y - b.Y, 2));
         }
         // Для того чтобы найти периметр многоугольника нам надо сложить все стороны. 
         public double PerimeterCalculator()
         {
             var perimeter = 0;
+            for (int i = 1; i < points.Length; i++)
+            {
+                perimeter = (int) LengthSide(points[i - 1], points[i]);
+            }
+
+            perimeter += (int) LengthSide(points[0], points[points.Length]);
             return perimeter;
         }
     }
--- Essential/PolygonPerimeter/PolygonPerimeter/Point.cs
+++ Essential/PolygonPerimeter/PolygonPerimeter/Point.cs
@@ -4,33 +4,21 @@ namespace PolygonPerimeter
 {
     class Point
     {
-        private int x, y;
-        private string name;
+        public int X { get; }
 
-        public int X
-        {
-            get { return x; }
-        }
+        public int Y { get; }
 
-        public int Y
-        {
-            get { return y; }
-        }
+        public string Name { get; }
 
-        public string Name
-        {
-            get { return name; }
-        }
-
-        //Пользовательский конструктор
-        public Point(int x, int y, string name)
+        //custom constructor
+        public Point()
         {
             Console.WriteLine("Enter X: ");
-            this.x = int.Parse(Console.ReadLine());
+            this.X = int.Parse(Console.ReadLine());
             Console.WriteLine("Enter Y: ");
-            this.y = int.Parse(Console.ReadLine());
+            this.Y = int.Parse(Console.ReadLine());
             Console.WriteLine("Enter Name: ");
-            this.name = Console.ReadLine();
+            this.Name = Console.ReadLine();
         }
     }
 }
