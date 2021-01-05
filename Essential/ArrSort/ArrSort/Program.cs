f8b7bbd11486600775cb1103d8ea287b173e3ace
Murmilyuk Artur <murmilyuk.artur20@gmail.com>
2021-01-05 19:26:24 +0200

    Sort arrays

---
+++ Essential/ArrSort/ArrSort/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ArrSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = {1, 3, 5, 7, 2, 0, 1, 2,6,5,8,23,77};
+
+            array.SortUp();
+            foreach (var numb in array)
+            {
+                Console.Write(numb + " ");
+            }
+        }
+    }
+}
