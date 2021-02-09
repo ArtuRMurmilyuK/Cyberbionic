c706f5eb812274b2fe046ad10da759148df858fa
ArtuRMurmilyuK <murmilyuk.artur20@gmail.com>
2021-02-09 16:24:05 +0200

    Create the program

---
+++ Essential/ArrayOfDelegates/ArrayOfDelegates/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ArrayOfDelegates
+{
+    class Program
+    {
+        public delegate int Average(int [] array);
+        static void Main()
+        {
+            Console.WriteLine(AverageDelegate());
+        }
+
+        public static int AverageDelegate()
+        {
+            Average average = delegate (int[] array)
+            {
+                int sum = 0;
+                for (int i = 0; i < array.Length; i++)
+                {
+                    sum += array[i];
+                }
+                return sum / array.Length;
+            };
+
+            int mid = average(new [] {22,2,3,5});
+
+            return mid;
+        }
+    }
+}
---
+++ Essential/ArrayOfDelegates/ArrayOfDelegates/readme.txt
@@ -0,0 +1,4 @@
+﻿ + Using Visual Studio, create a project using the Console Application template.
+ + Create an anonymous method that takes an array of delegates as an argument and 
+ + returns the arithmetic mean of the return values ​​of the methods reported with the delegates in the array. 
+ + Methods reported on delegates from an array return a random int value.
