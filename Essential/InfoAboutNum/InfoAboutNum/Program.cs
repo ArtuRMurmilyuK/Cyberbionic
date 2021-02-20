c1fa13df40f844687d1ad6f3da94eaa18d9f40a7
ArtuRMurmilyuK <murmilyuk.artur20@gmail.com>
2021-02-20 23:59:02 +0200

    Add remove method

---
+++ Essential/InfoAboutNum/InfoAboutNum/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+
+namespace InfoAboutNum
+{
+    class Program
+    {
+        static void Main()
+        {
+            var myList = new MyList<int>();
+            myList.Add(2);
+            myList.Add(3);
+            myList.Add(4);
+            myList.Add(5);
+            myList.Add(6);
+
+            myList.RemoveByIndex(1);
+            myList.RemoveByValue(6);
+
+            for (int i = 0; i < myList.Length; i++)
+            {
+                Console.WriteLine(myList.GetElement(i));
+            }
+
+            myList.SearchFoIndex(0);
+        }
+    }
+
+    public class MyList<T>
+    {
+        private T[] _array = new T[0];
+
+        public int Length => _array.Length;
+
+        public void Add(T arg)
+        {
+            T[] newArray = new T[_array.Length + 1];
+
+            for (int i = 0; i < newArray.Length; i++)
+            {
+                if (_array.Length != i)
+                {
+                    newArray[i] = _array[i];
+
+                    continue;
+                }
+                newArray[i] = arg;
+            }
+            _array = newArray;
+        }
+
+        public void SearchFoIndex(T index)
+        {
+            
+            for (int i = 0; i < _array.Length; i++)
+            {
+                if (Equals(index, i))
+                {
+                    var result =  _array[i];
+                    Console.WriteLine(result);
+                }
+            }
+        }
+        
+        public T GetElement(int index)
+        {
+            return _array[index];
+        }
+
+        public void RemoveByIndex(int index)
+        {
+            T[] newArray = new T[_array.Length-1];
+
+            for (int i = 0; i < index; i++)
+            {
+                newArray[i] = _array[i];
+            }
+
+            for (int i = index + 1; i < _array.Length; i++)
+            {
+                newArray[i - 1] = _array[i];
+            }
+
+            _array = newArray;
+        }
+
+        public void RemoveByValue(int value)
+        {
+            T[] newArray = new T[_array.Length - 1];
+
+            for (int i = 0; i < _array.Length; i++)
+            {
+                if (!Equals(_array[i], value))
+                {
+                    newArray[i] = _array[i];
+                }
+            }
+            _array = newArray;
+        }
+    }
+}
---
+++ Essential/InfoAboutNum/InfoAboutNum/readme.txt
@@ -0,0 +1,6 @@
+﻿Tas
+Create class MyList <T>. 
+Implement, in the simplest approximation, the possibility of using an instance of it similarly to an instance of the List <T> class. 
+The minimum required interface for interacting with an instance should include a method to add an item, 
+an indexer to get the value of an item at the specified index, 
+and a read-only property to get the total number of items.
