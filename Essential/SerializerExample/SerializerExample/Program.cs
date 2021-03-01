1313d21a1390b60ad0f3fd5834e9fcd11ebe93a8
ArtuRMurmilyuK <murmilyuk.artur20@gmail.com>
2021-03-01 16:02:12 +0200

    Create project

---
+++ Essential/SerializerExample/SerializerExample/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Text.Json;
+
+namespace SerializerExample
+{
+    class Program
+    {
+        static void Main()
+        {
+            Person tom=new Person{Name = "Anton", Age = 21};
+            string json = JsonSerializer.Serialize<Person>(tom);
+
+            Console.WriteLine(json);
+
+            Person restoredPerson = JsonSerializer.Deserialize<Person>(json);
+            Console.WriteLine(restoredPerson.Name);
+            Console.WriteLine(restoredPerson.Age);
+        }
+    }
+
+    class Person 
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
