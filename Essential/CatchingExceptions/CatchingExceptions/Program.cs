21c127a2023289514423c186791927eb858c88d7
ArtuRMurmilyuK <murmilyuk.artur20@gmail.com>
2021-03-01 15:18:50 +0200

    Exception catching

---
+++ Essential/CatchingExceptions/CatchingExceptions/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace CatchingExceptions
+{
+    class Program
+    {
+        static void Main()
+        {
+           EnterNumb();
+        }
+
+        static void EnterNumb()
+        {
+            try
+            {
+                Console.Write("Enter x: ");
+                int x = int.Parse(Console.ReadLine());
+
+                Console.Write("Enter y: ");
+                int y = int.Parse(Console.ReadLine());
+
+                int result = MyDel(x, y);
+                Console.WriteLine("Result: " + result);
+            }
+
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Div on 0 detected!");
+                Main();
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("this is not a number");
+                Main();
+            }
+        }
+
+        static int MyDel( int x,  int y)
+        {
+            return x / y;
+        }
+    }
+}
