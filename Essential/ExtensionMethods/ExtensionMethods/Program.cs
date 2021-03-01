8bdc27d18502efa66463159a57d4ff4f5e1ca989
ArtuRMurmilyuK <murmilyuk.artur20@gmail.com>
2021-03-01 15:01:13 +0200

    Create extension by string

---
+++ Essential/ExtensionMethods/ExtensionMethods/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ExtensionMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Ivan";
+
+            name.SayBy();
+            ExtensionClass.SayBy("Ann");
+        }
+    }
+
+    public static class ExtensionClass
+    {
+        public static void SayBy(this string s)
+        {
+            Console.WriteLine($"{s}, By!");
+        }
+    }
+}
