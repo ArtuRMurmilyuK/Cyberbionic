2996727850fc73f86b1522b9e64f7db54f812550
ArtuRMurmilyuK <murmilyuk.artur20@gmail.com>
2021-05-14 00:16:03 +0300

    Create the project Palinddrimes

---
+++ Essential/Palindromes/Palindromes/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Palindromes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] hoursString = new string[25];
+
+            for (int i = 0; i < 24; i++)
+            {
+                var moreTen = i < 10 ? ("0" + i).ToString() : i.ToString();
+
+                if (Int32.Parse(moreTen) >= 6 && Int32.Parse(moreTen) < 10 || 
+                    Int32.Parse(moreTen) >= 16 && Int32.Parse(moreTen) < 20)
+                {
+                    continue;
+                }
+
+                hoursString[i] = moreTen + ':' + moreTen[1]+moreTen[0];
+                Console.WriteLine(hoursString[i]);
+            }
+        }
+    }
+}
