2f73f0c537cd65d82e63ecbc1002a1f9192c9183
ArtuRMurmilyuK <murmilyuk.artur20@gmail.com>
2021-05-13 13:28:02 +0300

    odd and even numbers

---
+++ Essential/Arrays/Array2/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Array2
+{
+    class Program
+    {
+        static List<int> _evenNumbers = new List<int>();
+        static List<int> _oddNumbers = new List<int>();
+        static void Main(string[] args) // Результат: 2 массива, в одном четные числа из массивов,в  другом - нечет
+        {
+            int[] array1 = {1, 2, 4, 7, 9, 11, 12};
+
+            int[] array2 = {3, 5, 6, 7, 8, 9, 10};
+
+            int[] array3 = {2, 3, 6, 12, 13, 17, 18};
+
+            Even(array1);
+            Even(array2);
+            Even(array3);
+
+            Odd(array1);
+            Odd(array2);
+            Odd(array3);
+
+            Console.ReadKey();
+        }
+
+        public static void Even(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] % 2 == 0)
+                {
+                    _evenNumbers.Add(array[i]);
+                }
+            }
+        }
+
+        public static void Odd(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] % 2 == 1)
+                {
+                    _oddNumbers.Add(array[i]);
+                }
+            }
+        }
+    }
+}
---
+++ Essential/Arrays/Arrays/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+
+namespace Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var array1 = new int[]
+            {
+                1, 5, 7
+            };
+            var array2 = new int[]
+            {
+                2, 3, 8, 9
+            };
+
+            int[] array3 = new int[array1.Length + array2.Length];
+
+            for (int i = 0, a1Index = 0, a2Index = 0; i < array3.Length; i++)
+            {
+                if (a1Index < array1.Length && array1[a1Index] < array2[a2Index]   )
+                {
+                    array3[i] = array1[a1Index++];
+
+                    continue;
+                }
+
+                array3[i] = array2[a2Index++];
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
