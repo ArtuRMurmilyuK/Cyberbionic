2d5fbfc7ddefb9b0c87f1fb740eef7563c9fff60
ArtuRMurmilyuK <murmilyuk.artur20@gmail.com>
2021-10-01 21:34:10 +0300

    Create the project

---
+++ Essential/SumOfFractionsApp/SumOfFractionsApp/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace SumOfFractionsApp
+{
+    class Program
+    {
+        static void Main()
+        {
+        }
+    }
+}
---
+++ Essential/SumOfFractionsApp/SumOfFractionsAppTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace SumOfFractionsAppTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
