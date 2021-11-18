796459e8bd077d06c16d7329a72cf4f589b3e3aa
Artur Murmilyuk <murmilyuk.artur20@gmail.com>
2021-11-18 21:27:03 +0200

    Add class MyList and methods

---
+++ Essential/MyListApp/MyListApp/MyList.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace MyListApp
+{
+    public class MyList<T>
+    {
+        T[] _array;
+        public MyList()
+        {
+            _array = new T[0];
+        }
+
+        public void Add(T element)
+        {
+            T[] newArr = new T[_array.Length + 1];
+
+            for (int i = 0; i < newArr.Length; i++)
+            {
+                if (i < _array.Length)
+                {
+                    newArr[i] = _array[i];
+                }
+                else
+                {
+                    newArr[i] = element;
+                }
+            }
+            _array = newArr;
+        }
+
+        public void Show()
+        {
+            for (int i = 0; i < _array.Length; i++)
+            {
+                Console.WriteLine(_array[i]);
+            }
+        }
+
+        public void Insert(int index, T element)
+        {
+            T[] newArr = new T[_array.Length + 1];
+            
+            for (int i = 0; i < newArr.Length; i++)
+            {
+                T k = _array[index];
+                if (i == index)
+                {
+                    newArr[i] = element;
+                }
+                else if(i   > index)
+                {
+                    newArr[i] = k;
+                    index++;
+                }
+                else
+                {
+                    newArr[i] = _array[i];
+                }
+            }
+            _array = newArr;
+        }
+    }
+}
--- Essential/MyListApp/MyListApp/Program.cs
+++ Essential/MyListApp/MyListApp/Program.cs
@@ -1,10 +1,26 @@
-﻿namespace MyListApp
+﻿using System;
+
+namespace MyListApp
 {
     class Program
     {
         static void Main()
         {
-    
+            MyList<int> list = new MyList<int>();
+            
+            list.Add(2);
+            list.Add(3); 
+            list.Add(4);
+            list.Add(5);
+            
+            list.Show();
+            
+            list.Insert(0, 99);
+            list.Insert(2, 22);
+            
+            list.Show();
+
+            Console.ReadKey();
         }
     }
 }
