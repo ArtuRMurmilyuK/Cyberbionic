26cf410112a4b2aa3a922ae8d8098c8db6163686
ArtuRMurmilyuK <murmilyuk.artur20@gmail.com>
2022-01-12 18:12:37 +0200

    Add method with yield return

--- Essential/CollectionWithYield/CollectionWithYield/Program.cs
+++ Essential/CollectionWithYield/CollectionWithYield/Program.cs
@@ -1,9 +1,29 @@
-﻿namespace CollectionWithYield
+﻿using System;
+using System.Collections.Generic;
+
+namespace CollectionWithYield
 {
     internal class Program
     {
         static void Main()
         {
+            int[] array = new[] {1, 2, 3, 4, 5, 6, 7, 8, 9};
+            
+            foreach (var i in Collection(array))
+            {
+                Console.WriteLine(i);
+            }
+        }
+
+        static IEnumerable<int> Collection(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] % 2 == 0)
+                {
+                    yield return array[i];
+                }
+            }
         }
     }
 }
